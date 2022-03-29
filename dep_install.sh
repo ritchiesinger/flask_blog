c2d1aed0a9691468a3d895475aae595259c7ad13
Dmitriy Kruzhechkin <dmitriy.kruzhechkin@myoffice.team>
2022-03-30 01:01:06 +0300

    update README.md

--- dep_install.sh
+++ dep_install.sh
@@ -2,3 +2,4 @@ python -m pip install --upgrade pip
 python -m pip install --upgrade setuptools
 python -m pip install --upgrade wheel
 pip install -r requirements.txt
+pip install -r requirements-dev.txt
