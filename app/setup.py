8a0f90c38e7ccfc94b450249282367725ca4b205
Dmitriy Kruzhechkin <dmitriy.kruzhechkin@myoffice.team>
2022-04-02 01:20:41 +0300

    fix Flask version in setup

--- app/setup.py
+++ app/setup.py
@@ -7,7 +7,7 @@ setup(
     packages=[],
     include_package_data=True,
     install_requires=[
-        'Flask==2.0.3',
+        'Flask==2.1.1',
         'passlib==1.7.4',
         'python-dotenv==0.20.0',
         'PyJWT==2.3.0',
